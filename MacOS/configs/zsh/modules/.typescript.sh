88e6f6c5f034af785f95181873672d713d5bb908
Gareth Penrose <gareth@chetwood.co>
2026-09-23 17:34:04 +0100

    fix(macos): Fix typescript zsh config

--- MacOS/configs/zsh/modules/.typescript.sh
+++ MacOS/configs/zsh/modules/.typescript.sh
@@ -5,20 +5,14 @@
 # path_append "$HOME/.deno/bin"
 
 # nvm
+command -v brew >/dev/null 2>&1 ||
+{ echo "Homebrew is not installed or not on PATH"; return 1; }
+HOMEBREW_PREFIX="$(brew --prefix)"
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+: "${HOMEBREW_PREFIX:?HOMEBREW_PREFIX is not set}"
 [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm (WITH BREW PREFIX)
 [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion (WITH BREW PREFIX)
-
-# pnpm
-
-# Mise (pnpm)
-eval "$(/opt/homebrew/bin/mise activate zsh)"
-
-export PNPM_HOME="$HOME/.local/share/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-# pnpm end
 
 # Allow basic lodash emulation in terminal
 lodash() {
