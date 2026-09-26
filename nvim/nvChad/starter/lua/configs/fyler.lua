fb50cd12ebce7571ca55964472f91f127d6a706c
Gareth Penrose <garethpenrose@Gareths-MacBook-Pro.local>
2026-09-26 13:29:26 +0100

    fix(nvim): fyler - config - tweak hotkeys

--- nvim/nvChad/starter/lua/configs/fyler.lua
+++ nvim/nvChad/starter/lua/configs/fyler.lua
@@ -1,13 +1,21 @@
-function open_fyler () 
-  return require("fyler").open()
+function open_fyler(args)
+  return function() require("fyler").open(args or {}) end
 end
 
 local M = {
+  keys = {
+    { "<leader>b", open_fyler(), desc = "Open Fyler View" },
+    { "<C-b>", open_fyler({ kind = "split_left" }), desc = "Open Fyler View - Sidebar" },
+  },
   opts = {
     kind = "floating",
     follow_current_file = true,
     integrations = {
       icon = 'nvim_web_devicons'
+    },
+    kind_presets = {
+      split_left = { width = '25%' },
+      split_left_most = { width = '25%' },
     },
     mappings = {
       n = {
@@ -16,9 +24,6 @@ local M = {
         },
       },
     },
-  },
-  keys = {
-    { "<C-b>", open_fyler, desc = "Open Fyler View" },
   },
 }
 
